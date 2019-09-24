13c48f320f4f5636f96c3e8c39009c635c91d50f
JohnDi0505 <chongdi0505@gmail.com>
2019-09-24 12:35:17 -0400

    Add files via upload

---
+++ setup_local_postgresql_database.sql
@@ -0,0 +1,45 @@
+CREATE EXTENSION address_standardizer;
+CREATE EXTENSION fuzzystrmatch;
+CREATE EXTENSION ogr_fdw;
+CREATE EXTENSION pgrouting;
+CREATE EXTENSION plpgsql;
+CREATE EXTENSION postgis;
+CREATE EXTENSION postgis_sfcgal;
+CREATE EXTENSION postgis_tiger_geocoder;
+CREATE EXTENSION postgis_topology;
+
+CREATE TABLE geocd_nj (
+	ticket INT PRIMARY KEY,
+	dt DATE NOT NULL,
+	tm TIME NOT NULL,
+	rating SMALLINT,
+	hder VARCHAR (20),
+	lon DOUBLE PRECISION,
+	lat DOUBLE PRECISION,
+	st_no SMALLINT,
+	st_nm VARCHAR (50),
+	city VARCHAR (50),
+	county VARCHAR (50),
+	state_nm VARCHAR (5),
+	zip VARCHAR (5),
+	excav_comp VARCHAR (50),
+	inter1 VARCHAR (50),
+	inter2 VARCHAR (50)
+);
+
+INSERT INTO geocd_nj (ticket, dt, tm, hder, county, excav_comp, inter1, inter2)
+	
+	VALUES (100600001,'2010-03-01', '02:51:21-05', 'EMERGENCY', 'BERGEN', 'ORANGE AND ROCKLAND UTILITIES', 'HARING FARM', NULL)
+;
+
+UPDATE geocd_nj
+	SET 
+	rating = g.rating, lon = ST_X(g.geomout),lat = ST_Y(g.geomout),
+    st_no = (addy).address,st_nm = (addy).streetname,
+    city = (addy).location,state_nm = (addy).stateabbrev,zip = (addy).zip
+	FROM geocode('9 PIERMONT, ROCKLEIGH, NJ', 1) As g
+	
+	WHERE ticket = 100600001
+;
+
+SELECT * FROM geocd_nj;
